3a4b6a834c90ddf7df405167f8a92d92a146e747
ncannasse <ncannasse@9c6bba75-186d-22db-1431-afedb3bea791>
2004-10-20 13:24:30 +0000

    forked from mtypes.

---
+++ expr.ml
@@ -0,0 +1,281 @@
+
+type pos = {
+	pfile : string;
+	pmin : int;
+	pmax : int;
+}
+
+type keyword =
+	| Function
+	| Class
+	| Var
+	| If
+	| Else
+	| While
+	| Do
+	| For
+	| Break
+	| Continue
+	| Return
+	| Interface
+	| Extends
+	| Implements
+	| Import
+	| Switch
+	| Case
+	| Default
+	| Static
+	| Intrinsic
+	| Dynamic
+	| Public
+	| Private
+
+type binop =
+	| OpAdd
+	| OpMult
+	| OpDiv
+	| OpSub
+	| OpAssign
+	| OpEq
+	| OpNotEq
+	| OpGt
+	| OpGte
+	| OpLt
+	| OpLte
+	| OpAnd
+	| OpOr
+	| OpXor
+	| OpBoolAnd
+	| OpBoolOr
+	| OpShl
+	| OpShr
+	| OpUShr
+	| OpMod
+	| OpAssignOp of binop
+
+type unop =
+	| Increment
+	| Decrement
+	| Not
+	| Neg
+	| NegBits
+
+type constant =
+	| Int of string
+	| Float of string
+	| String of string
+	| Ident of string
+	| Name of string
+
+type token =
+	| Eof
+	| Const of constant
+	| Kwd of keyword
+	| Comment of string
+	| CommentLine of string
+	| Binop of binop
+	| Unop of unop
+	| Next
+	| Sep
+	| BrOpen
+	| BrClose
+	| BkOpen
+	| BkClose
+	| POpen
+	| PClose
+	| Dot
+	| DblDot
+	| Question
+	| Arrow
+	| Quote of string
+
+type unop_flag =
+	| Prefix
+	| Postfix
+
+type while_flag =
+	| NormalWhile
+	| DoWhile
+
+type static_flag = 
+	| IsMember
+	| IsStatic
+
+type public_flag =
+	| IsPublic
+	| IsPrivate
+
+type type_path = string list * string
+
+type func = {
+	fname : string;
+	fargs : (string * type_path option) list;
+	ftype : type_path option;
+	fstatic : static_flag;
+	fpublic : public_flag;
+	fexpr : expr option;
+}
+
+and herit = 
+	| HExtends of type_path
+	| HImplements of type_path
+	| HIntrinsic
+	| HDynamic
+
+and eval_def =
+	| EConst of constant
+	| EArray of eval * eval
+	| EBinop of binop * eval * eval
+	| EField of eval * string
+	| EStatic of type_path
+	| EParenthesis of eval
+	| EObjDecl of (string * eval) list
+	| EArrayDecl of eval list
+	| ECall of eval * eval list
+	| ENew of type_path * eval list
+	| EUnop of unop * unop_flag * eval
+	| EQuestion of eval * eval * eval
+	| ELambda of func
+
+and eval = eval_def * pos
+
+and expr_def =
+	| EVars of static_flag * public_flag * (string * type_path option * eval option) list
+	| EFunction of func
+	| EBlock of expr list
+	| EFor of expr list * eval list * eval list * expr
+	| EForIn of expr * eval * expr
+	| EIf of eval * expr * expr option
+	| EWhile of eval * expr * while_flag
+	| ESwitch of eval * (eval * expr) list * expr option
+	| EReturn of eval option
+	| EBreak
+	| EContinue
+	| EVal of eval
+
+and expr = expr_def * pos
+
+and sign_def =
+	| EClass of type_path * herit list * expr
+	| EInterface of type_path * herit list * expr
+	| EImport of type_path
+
+and signature = sign_def * pos
+
+let pos = snd
+
+let is_postfix = function
+	| Increment | Decrement -> true
+	| Not | Neg | NegBits -> false
+
+let is_prefix = function
+	| Increment | Decrement -> true
+	| Not | Neg | NegBits -> true
+
+let base_class_name = snd
+
+let null_pos = { pfile = "<null>"; pmin = -1; pmax = -1 }
+
+let punion p p2 =
+	{
+		pfile = p.pfile;
+		pmin = min p.pmin p2.pmin;
+		pmax = max p.pmax p2.pmax;
+	}
+
+let s_type_path (p,s) = match p with [] -> s | _ -> String.concat "." p ^ "." ^ s
+
+let s_escape s =
+	let b = Buffer.create (String.length s) in
+	for i = 0 to (String.length s) - 1 do
+		match s.[i] with
+		| '\n' -> Buffer.add_string b "\\n"
+		| '\t' -> Buffer.add_string b "\\t"
+		| '\r' -> Buffer.add_string b "\\r"
+		| c -> Buffer.add_char b c
+	done;
+	Buffer.contents b
+
+let s_constant = function
+	| Int s -> s
+	| Float s -> s
+	| String s -> "\"" ^ s_escape s ^ "\""
+	| Ident s -> s
+	| Name s -> s
+	
+let s_keyword = function
+	| Function -> "function"
+	| Class -> "class"
+	| Static -> "static"
+	| Var -> "var"
+	| If -> "if"
+	| Else -> "else"
+	| While -> "while"
+	| Do -> "do"
+	| For -> "for"
+	| Break -> "break"
+	| Return -> "return"
+	| Continue -> "continue"
+	| Interface -> "interface"
+	| Extends -> "extends"
+	| Implements -> "implements"
+	| Import -> "import"
+	| Switch -> "switch"
+	| Case -> "case"
+	| Default -> "default"
+	| Intrinsic -> "intrinsic"
+	| Dynamic -> "dynamic"
+	| Private -> "private"
+	| Public -> "public"
+
+let rec s_binop = function
+	| OpAdd -> "+"
+	| OpMult -> "*"
+	| OpDiv -> "/"
+	| OpSub -> "-"
+	| OpAssign -> "="
+	| OpEq -> "=="
+	| OpNotEq -> "!="
+	| OpGte -> ">="
+	| OpLte -> "<="
+	| OpGt -> ">"
+	| OpLt -> "<"
+	| OpAnd -> "&"
+	| OpOr -> "|"
+	| OpXor -> "^"
+	| OpBoolAnd -> "&&"
+	| OpBoolOr -> "||"
+	| OpShr -> ">>"
+	| OpUShr -> ">>>"
+	| OpShl -> "<<"
+	| OpMod -> "%"
+	| OpAssignOp op -> s_binop op ^ "="
+
+let s_unop = function
+	| Increment -> "++"
+	| Decrement -> "--"
+	| Not -> "!"
+	| Neg -> "-"
+	| NegBits -> "~"
+
+let s_token = function
+	| Eof -> "<end of file>"
+	| Const c -> s_constant c
+	| Kwd k -> s_keyword k
+	| Comment s -> "/*"^s^"*/"
+	| CommentLine s -> "//"^s
+	| Binop o -> s_binop o
+	| Unop o -> s_unop o
+	| Next -> ";"
+	| Sep -> ","
+	| BkOpen -> "["
+	| BkClose -> "]"
+	| BrOpen -> "{"
+	| BrClose -> "}"
+	| POpen -> "("
+	| PClose -> ")"
+	| Dot -> "."
+	| DblDot -> ":"
+	| Question -> "?"
+	| Arrow -> "->"
+	| Quote s -> s 
---
+++ parser.ml
@@ -0,0 +1,281 @@
+open Expr
+
+type error_msg =
+	| Unexpected of token
+	| Unclosed_parenthesis
+	| Duplicate_default
+
+exception Error of error_msg * pos
+
+let error_msg = function
+	| Unexpected t -> "Unexpected "^(s_token t)
+	| Unclosed_parenthesis -> "Unclosed parenthesis"
+	| Duplicate_default -> "Duplicate default"
+
+let error m p = raise (Error (m,p))
+
+let priority = function
+	| OpAssign | OpAssignOp _ -> -3
+	| OpBoolAnd | OpBoolOr -> -2
+	| OpEq | OpNotEq | OpGt | OpLt | OpGte | OpLte -> -1
+	| OpAdd | OpSub -> 0
+	| OpMult | OpDiv -> 1
+	| OpOr | OpAnd | OpXor -> 2
+	| OpShl | OpShr | OpMod | OpUShr -> 3
+
+let rec make_binop op e ((v,p2) as e2) =
+	match v with
+	| EBinop (_op,_e,_e2) when priority _op <= priority op ->
+		let _e = make_binop op e _e in
+		EBinop (_op,_e,_e2) , punion (pos _e) (pos _e2)
+	| _ ->
+		EBinop (op,e,e2) , punion (pos e) (pos e2)
+
+let rec make_unop op ((v,p2) as e) p1 = 
+	match v with
+	| EBinop (bop,e,e2) -> EBinop (bop, make_unop op e p1 , e2) , (punion p1 p2)
+	| _ ->
+		EUnop (op,Prefix,e), punion p1 p2
+
+let rec make_path e = 
+	let rec loop acc (e,_) =
+		match e with
+		| EConst (Ident s) -> s :: acc
+		| EField (e,f) -> loop (f :: acc) e
+		| _ -> raise Stream.Failure
+	in
+	loop [] e
+
+let rec	parse_code = parser
+	| [< '(Eof,_) >] -> []
+	| [< '(Next,_); el = parse_code >] -> el
+	| [< e = parse_signature; el = parse_code >] -> e :: el
+
+and parse_signature = parser
+	| [< '(Kwd Import,p1); p = parse_class_path >] -> EImport p , p1
+	| [< '(Kwd Interface,p1); path = parse_class_path; herits = parse_herits; '(BrOpen,p); el , p2 = parse_class parse_field_decl >] -> 
+		EInterface (path,herits,(EBlock el,punion p p2)) , punion p1 p2
+	| [< flags = parse_class_flags; '(Kwd Class,p); path = parse_class_path; herits = parse_herits; '(BrOpen,op); s >] -> 
+		let el, p2 = parse_class (if List.exists ((=) HIntrinsic) flags then parse_field_impl else parse_field_decl) s in
+		EClass (path, flags @ herits, (EBlock el, punion op p2)) , punion p p2
+
+and parse_herits = parser
+	| [< '(Kwd Extends,_); p = parse_class_path; l = parse_herits >] -> HExtends p :: l
+	| [< '(Kwd Implements,_); p = parse_class_path; l = parse_herits >] -> HImplements p :: l
+	| [< >] -> []
+
+and parse_class_flags = parser
+	| [< '(Kwd Intrinsic,_); l = parse_class_flags >] -> HIntrinsic :: l
+	| [< '(Kwd Dynamic,_); l = parse_class_flags >] -> HDynamic :: l
+	| [< >] -> []
+
+and parse_class pfield = parser
+	| [< '(BrClose,p) >] -> [] , p
+	| [< '(Next,_); n = parse_class pfield >] -> n
+	| [< flags = parse_field_flags IsMember IsPublic; f = pfield flags; fl , p = parse_class pfield >] -> f :: fl , p
+
+and parse_field_flags stat pub = parser
+	| [< '(Kwd Static,_); f = parse_field_flags IsStatic pub >] -> f
+	| [< '(Kwd Public,_); f = parse_field_flags stat IsPublic >] -> f
+	| [< '(Kwd Private,_); f = parse_field_flags stat IsPrivate >] -> f
+	| [< >] -> stat , pub
+
+and parse_field_impl (stat,pub) = parser
+	| [< '(Kwd Var,p1); vl, p2 = parse_vars p1 >] -> EVars (stat,pub,vl) , punion p1 p2
+	| [< '(Kwd Function,p1); '(Const (Ident fname),_); '(POpen,_); args , p2 = parse_args; t = parse_type_option; e = parse_expr >] -> 
+		EFunction {
+			fname = fname;
+			fargs = args;
+			ftype = t;
+			fstatic = stat;
+			fpublic = pub;
+			fexpr = Some e;
+		} , punion p1 (pos e)
+
+and parse_field_decl (stat,pub) = parser
+	| [< '(Kwd Var,p1); vl, p2 = parse_vars p1;  >] -> EVars (stat,pub,vl), punion p1 p2
+	| [< '(Kwd Function,p1); '(Const (Ident fname),_); '(POpen,_); args , p2 = parse_args; t = parse_type_option >] ->
+		EFunction {	
+			fname = fname;
+			fargs = args;
+			ftype = t;
+			fstatic = stat;
+			fpublic = pub;
+			fexpr = None;
+		} , punion p1 p2
+
+and parse_expr = parser
+	| [< '(BrOpen,p1); el , p2 = parse_block parse_expr p1 >] -> EBlock el , punion p1 p2
+	| [< '(Kwd For,p); '(POpen,_); e = parse_for p >] -> e
+	| [< '(Kwd If,p); cond = parse_eval; e = parse_expr; e2 , p2 = parse_else (pos e) >] -> EIf (cond,e,e2), punion p p2
+	| [< '(Kwd Return,p); v , p2 = parse_eval_option p; >] -> EReturn v , punion p p2
+	| [< '(Kwd Break,p); >] -> EBreak , p
+	| [< '(Kwd Continue,p); >] -> EContinue , p
+	| [< '(Kwd While,p1); v = parse_eval; e = parse_expr >] -> EWhile (v,e,NormalWhile) , punion p1 (pos e)
+	| [< '(Kwd Do,p1); e = parse_expr; '(Kwd While,_); v = parse_eval; >] -> EWhile (v,e,DoWhile) , punion p1 (pos v)
+	| [< '(Kwd Switch,p1); v = parse_eval; '(BrOpen,_); el , eo, p2 = parse_switch >] -> ESwitch (v,el,eo) , punion p1 p2
+	| [< '(Kwd Var,p1); vl, p2 = parse_vars p1 >] -> EVars (IsMember,IsPublic,vl), punion p1 p2
+	| [< e = parse_eval >] -> EVal e , pos e
+
+and parse_eval = parser
+	| [< '(Const (Ident "fun"),p1); '(POpen,_); args, _ = parse_args; t = parse_type_option; e = parse_expr;
+		v = parse_eval_next (ELambda {
+			fname = "";
+			fargs = args;
+			ftype = t;
+			fstatic = IsStatic;
+			fpublic = IsPublic;
+			fexpr = Some e;
+		} , punion p1 (pos e)) >] -> v
+	| [< '(Const (Ident "new"),p1); p = parse_class_path; e = parse_new p p1 >] -> e
+	| [< '(Const c,p); e = parse_eval_next (EConst c,p)  >] -> e
+	| [< '(POpen,p1); e = parse_eval; '(PClose,p2); e = parse_eval_next (EParenthesis e , punion p1 p2) >] -> e
+	| [< '(BrOpen,p1); el, p2 = parse_field_list; e = parse_eval_next (EObjDecl el, punion p1 p2) >] -> e
+	| [< '(BkOpen,p1); el, p2 = parse_array; e = parse_eval_next (EArrayDecl el,punion p1 p2) >] -> e
+	| [< '(Unop op,p1) when is_prefix op; e = parse_eval >] -> make_unop op e p1
+	| [< '(Binop OpSub,p1); e = parse_eval >] -> make_unop Neg e p1
+
+and parse_eval_next e = parser
+	| [< '(BkOpen,_); e2 = parse_eval; '(BkClose,p2); e = parse_eval_next (EArray (e,e2) , punion (pos e) p2) >] -> e
+	| [< op = parse_binop; e2 = parse_eval; >] -> make_binop op e e2
+	| [< '(Dot,_); e = parse_field_access e >] -> e
+	| [< '(POpen,_); args = parse_eval_list; '(PClose,p2); e = parse_eval_next (ECall (e,args), punion (pos e) p2) >] -> e
+	| [< '(Unop op,p2) when is_postfix op; e = parse_eval_next (EUnop (op,Postfix,e), punion (pos e) p2) >] -> e
+	| [< '(Question,_); v1 = parse_eval; '(DblDot,_); v2 = parse_eval; e = parse_eval_next (EQuestion (e,v1,v2), punion (pos e) (pos v2)) >] -> e
+	| [< >] -> e
+
+and parse_field_access e = parser
+	| [< '(Const (Ident field),p2); e = parse_eval_next (EField (e,field), punion (pos e) p2) >] -> e
+	| [< '(Const (Name cl),p2); e = parse_eval_next (EStatic (make_path e,cl), punion (pos e) p2) >] -> e
+
+and parse_binop = parser
+	| [< '(Binop OpGt,_); op = parse_binop_gt >] -> op
+	| [< '(Binop op,_) >] -> op
+
+and parse_binop_gt = parser
+	| [< '(Binop OpGt,_); op = parse_binop_gt2 >] -> op
+	| [< >] -> OpGt
+
+and parse_binop_gt2 = parser
+	| [< '(Binop OpGt,_) >] -> OpUShr
+	| [< >] -> OpShr
+
+and parse_new e p1 = parser
+	| [< '(POpen,_); args = parse_eval_list; '(PClose,p2); e = parse_eval_next (ENew (e,args), punion p1 p2) >] -> e	
+
+and parse_expr_option p = parser
+	| [< e = parse_expr >] -> Some e , pos e
+	| [< >] -> None, p
+
+and parse_eval_option p = parser
+	| [< v = parse_eval >] -> Some v , pos v
+	| [< >] -> None, p
+
+and parse_eval_list = parser
+	| [< v = parse_eval; vl = parse_eval_list >] -> v :: vl
+	| [< '(Sep,_); vl = parse_eval_list >] -> vl
+	| [< '(Next,_) >] -> []
+	| [< >] -> []
+
+and parse_field_list = parser
+	| [< '(Const (Ident fname),_); '(DblDot,_); e = parse_eval; el , p = parse_field_list >] -> (fname,e) :: el , p
+	| [< '(Sep,_); el = parse_field_list >] -> el
+	| [< '(BrClose,p) >] -> [] , p
+
+and parse_array = parser
+	| [< e = parse_eval; el , p = parse_array >] -> e :: el , p
+	| [< '(Sep,_); e = parse_array >] -> e
+	| [< '(BkClose,p) >] -> [] , p
+
+and parse_else p = parser
+	| [< '(Next,_); e = parse_else p >] -> e
+	| [< '(Kwd Else,_); e = parse_expr >] -> Some e, pos e
+	| [< >] -> None , p
+
+and parse_for p = parser
+	| [< cl = parse_for_conds; l1 = parse_eval_list; l2 = parse_eval_list; '(PClose,p2); e = parse_expr >] -> EFor(cl,l1,l2,e) , punion p p2
+
+and parse_for_conds = parser
+	| [< e = parse_expr; l = parse_for_conds_next >] -> e :: l
+
+and parse_for_conds_next = parser
+	| [< '(Sep,_); l = parse_for_conds >] -> l
+	| [< '(Next,_) >] -> [] 
+	| [< >] -> []
+
+and parse_args = parser
+	| [< '(Const (Ident name),_); t = parse_type_option; al , p = parse_args >] -> (name , t) :: al , p
+	| [< '(Sep,_); al= parse_args >] -> al
+	| [< '(PClose,p) >] -> [] , p
+
+and parse_vars p = parser
+	| [< '(Const (Ident name),p); t = parse_type_option; v = parse_var_init; vl , p = parse_vars_next p >] -> (name , t, v) :: vl , p
+	| [< >] -> [] , p
+
+and parse_vars_next p = parser
+	| [< '(Sep,_); vl , p = parse_vars p >] -> vl , p
+	| [< >] -> [] , p
+
+and parse_var_init = parser
+	| [< '(Binop OpAssign,_); v = parse_eval >] -> Some v
+	| [< >] -> None
+
+and parse_switch = parser
+	| [< '(BrClose,p) >] -> [] , None , p
+	| [< '(Kwd Case,p); v = parse_eval; '(DblDot,_); c = parse_switch_clause; el, eo, p2 = parse_switch >] -> (v,(EBlock c,p)) :: el , eo , p2
+	| [< '(Kwd Default,p); '(DblDot,_); c = parse_switch_clause; el, eo, p2 = parse_switch >] -> 
+		if eo <> None then error Duplicate_default p;
+		el , Some (EBlock c,p) , p2
+
+and parse_switch_clause = parser
+	| [< e = parse_expr; el = parse_switch_clause >] -> e :: el
+	| [< '(Next,_); el = parse_switch_clause >] -> el
+	| [< >] -> []
+
+and parse_block callb sp = parser
+	| [< e = callb; el,p = parse_block callb sp >] -> e :: el , p
+	| [< '(Next,_); el = parse_block callb sp >] -> el
+	| [< '(BrClose,p); >] -> [] , p
+	| [< '(Eof,_) >] -> error Unclosed_parenthesis sp
+
+and parse_expr_list p = parser
+	| [< e = parse_expr; el, p = parse_expr_list (pos e) >] -> e :: el , p
+	| [< '(Next,_); el = parse_expr_list p >] -> el
+	| [< >] -> [] , p
+
+and parse_type_option = parser
+	| [< '(DblDot,_); t = parse_class_path >] -> Some t
+	| [< >] -> None
+
+and parse_class_path = parser
+	| [< '(Const (Ident name),_); '(Dot,_); path , cname = parse_class_path  >] -> name :: path , cname
+	| [< '(Const (Name name),_) >] -> [] , name
+
+
+let parse code file =
+	let old = Lexer.save() in
+	Lexer.init file;
+	let last = ref (Eof,null_pos) in
+	let comments = ref [] in
+	let rec next_token x =
+		let t, p = Lexer.token code in
+		match t with 
+		| Comment s | CommentLine s -> 
+			comments := (s,p) :: !comments;
+			next_token x
+		| _ ->
+			last := (t , p);
+			Some (t , p)
+	in
+	try
+		let l = parse_code (Stream.from next_token) in
+		Lexer.restore old;
+		l , List.rev !comments
+	with
+		| Stream.Error _
+		| Stream.Failure -> 
+			Lexer.restore old;
+			error (Unexpected (fst !last)) (pos !last)
+		| e ->
+			Lexer.restore old;
+			raise e
