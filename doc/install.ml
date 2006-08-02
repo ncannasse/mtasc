f5fa67b7c08aa30943800e1d0a9837ba99f6b90b
ncannasse <ncannasse@9c6bba75-186d-22db-1431-afedb3bea791>
2006-08-02 16:00:33 +0000

    updated

--- doc/install.ml
+++ doc/install.ml
@@ -86,7 +86,7 @@ let compile() =
 
 	(* SWFLIB *)
 	Sys.chdir "ocaml/swflib";
-	let files = "-I .. -I ../extc swf.ml swfZip.ml actionScript.ml swfParser.ml" in
+	let files = "-I .. -I ../extc as3.mli as3code.ml as3parse.ml swf.ml swfZip.ml actionScript.ml swfParser.ml" in
 	if bytecode then command ("ocamlc -a -o swflib.cma " ^ files);
 	if native then command ("ocamlopt -a -o swflib.cmxa " ^ files);
 	Sys.chdir "../..";
